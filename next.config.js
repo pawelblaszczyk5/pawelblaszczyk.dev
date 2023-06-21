5a25bdc707db0179e53002bbfda15e1fdab0b760
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-21 23:08:13 +0200

    Cleanup boilerplate

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+    serverActions: true,
+  },
+  eslint: {
+    dirs: ["."],
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
