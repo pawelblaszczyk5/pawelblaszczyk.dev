ad72a11eaa60fa85f6dfc86122bd8a8987da2bcd
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-02 19:44:09 +0200

    Adjust preview options

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,10 +5,16 @@ import "~/app.css";
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: "^on[A-Z].*" },
+		backgrounds: { disable: true },
 		controls: {
 			matchers: {
 				color: /(background|color)$/iu,
 				date: /Date$/u,
+			},
+		},
+		docs: {
+			story: {
+				autoplay: true,
 			},
 		},
 	},
