15c3191a754dd271a84eb47f6623f8d2af4799f2
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-02-25 19:23:59 +0100

    Test change

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -3,7 +3,7 @@ import { getCurrentUser } from "@blog/data/auth";
 import { Button } from "@blog/design-system/button";
 
 const HomePage = () => (
-	<h1 style={css({ "--hover_color": "var(---, red)" })}>
+	<h1 style={css({ "--hover_color": "var(---, blue)" })}>
 		Hello world {getCurrentUser()}
 		<Button />
 	</h1>
