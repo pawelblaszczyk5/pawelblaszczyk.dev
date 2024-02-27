07db456e44c60feb6f9635a123e301a02b3dd1dc
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-02-27 20:02:23 +0100

    Automatic app deployment

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,8 +1,17 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const monorepoRootDirectory = join(dirname(fileURLToPath(import.meta.url)), "../../");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	experimental: {
+		outputFileTracingRoot: monorepoRootDirectory,
+	},
+	output: "standalone",
 	transpilePackages: ["@blog/data", "@blog/css", "@blog/design-system", "@blog/css-reset"],
 	typescript: {
 		ignoreBuildErrors: true,
