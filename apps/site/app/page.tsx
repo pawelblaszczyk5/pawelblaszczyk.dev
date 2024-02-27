af81f8ce62ffc5c4d4e378f9179a90cdac9e9158
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-02-27 20:32:54 +0100

    Random update to test deployment

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@blog/design-system/button";
 
 const HomePage = () => (
 	<h1 style={css({ "--color": "var(---, blue)" })}>
-		Hello world
+		Hello world!
 		<Button />
 		<span>Current user: {getCurrentUser()}</span>
 	</h1>
