789f5569e4bd63391dcfb02ff523de26fe77951a
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-02-27 21:49:45 +0100

    Add some update

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,5 +1,5 @@
 import { css } from "@blog/css";
 
-const HomePage = () => <h1 style={css({ "--color": "var(---, #2f81f7)" })}>Hello world from preview!</h1>;
+const HomePage = () => <h1 style={css({ "--color": "var(---, #2f81f7)" })}>Hello world from preview updated!</h1>;
 
 export default HomePage;
