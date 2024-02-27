9fce55c5dc921af9df01e308fad288a6474a16e9
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-02-27 21:25:13 +0100

    Update copy

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,13 +1,5 @@
 import { css } from "@blog/css";
-import { getCurrentUser } from "@blog/data/auth";
-import { Button } from "@blog/design-system/button";
 
-const HomePage = () => (
-	<h1 style={css({ "--color": "var(---, blue)" })}>
-		Hello world!
-		<Button />
-		<span>Current user: {getCurrentUser()}</span>
-	</h1>
-);
+const HomePage = () => <h1 style={css({ "--color": "var(---, #2f81f7)" })}>Hello world from preview!</h1>;
 
 export default HomePage;
