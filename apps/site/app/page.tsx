c3a612ebd3fb5cf698e4a41b43a95b8ea2cba786
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-03-03 01:25:48 +0100

    Change color

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -57,7 +57,7 @@ const HomePage = () => (
 			}}
 		>
 			<input name="text" type="text" />
-			<span style={css({ "--color": "var(---, blue)" })}>{getRandomNumber()}</span>
+			<span style={css({ "--color": "var(---, red)" })}>{getRandomNumber()}</span>
 			<button
 				formAction={async () => {
 					"use server";
