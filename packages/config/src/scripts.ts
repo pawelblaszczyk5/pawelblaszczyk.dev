caf7feb5f97195e05450f56f2fd467459cede1f5
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-03-03 01:12:23 +0100

    Rename fly env variable

--- packages/config/src/scripts.ts
+++ packages/config/src/scripts.ts
@@ -1,7 +1,7 @@
 import * as v from "valibot";
 
 const configSchema = v.object({
-	FLY_ACCESS_TOKEN: v.string(),
+	FLY_API_TOKEN: v.string(),
 	TURBO_TEAM: v.string(),
 	TURBO_TOKEN: v.string(),
 });
@@ -9,7 +9,7 @@ const configSchema = v.object({
 type Config = v.Output<typeof configSchema>;
 
 const maybeConfig = {
-	FLY_ACCESS_TOKEN: process.env["FLY_ACCESS_TOKEN"],
+	FLY_API_TOKEN: process.env["FLY_API_TOKEN"],
 	TURBO_TEAM: process.env["TURBO_TEAM"],
 	TURBO_TOKEN: process.env["TURBO_TOKEN"],
 } satisfies Record<keyof Config, unknown>;
