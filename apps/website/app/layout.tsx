e5b9ab711a2e175cf75eee0570f234915defb247
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-03-06 20:17:36 +0100

    Add root container with isolation

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 
+import { css } from "@pawelblaszczyk.dev/css";
+
 import "#src/app/main.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +24,9 @@ const RootLayout = ({
 	children: React.ReactNode;
 }>) => (
 	<html lang="en">
-		<body className={inter.className}>{children}</body>
+		<body className={inter.className}>
+			<div style={css({ "--isolation": "isolate" })}>{children}</div>
+		</body>
 	</html>
 );
 
