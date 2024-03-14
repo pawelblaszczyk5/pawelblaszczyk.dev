cfaffec059d33d989c4dd49bc469b8ada8c48406
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-03-14 20:17:45 +0100

    Mock page content

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,4 +1,5 @@
-import { revalidatePath, revalidateTag, unstable_cache } from "next/cache";
+import { revalidateTag, unstable_cache } from "next/cache";
+import { cookies } from "next/headers";
 import { Suspense } from "react";
 
 import { css } from "@pawelblaszczyk.dev/css";
@@ -13,65 +14,74 @@ const sleep = async (time: number) => {
 	});
 };
 
-const getRandomNumber = unstable_cache(
-	async () => {
-		await sleep(1_000);
+const getEntries = unstable_cache(async () => await database.select().from(entries), ["entries"], {
+	tags: ["entries"],
+});
 
-		return Math.random();
-	},
-	["random-number"],
-	{
-		tags: ["random-number"],
-	},
-);
-
-const List = async () => {
-	const data = await database.select().from(entries);
+const User = async () => {
+	const currentUser = cookies().get("username")?.value;
 
 	await sleep(1_000);
 
 	return (
 		<div>
-			{data.map(row => (
-				<p key={row.id}>
-					{row.id} - {row.text}
-				</p>
-			))}
+			<p>{currentUser ? `Currently logged in as: ${currentUser}` : "Currently anonymous"}</p>
+			<form
+				action={async formData => {
+					"use server";
+
+					await sleep(500);
+
+					const username = formData.get("username") as string;
+
+					cookies().set({ name: "username", value: username });
+				}}
+			>
+				<input name="username" />
+				<button type="submit">Change username</button>
+			</form>
 		</div>
+	);
+};
+
+const Entries = async () => {
+	const data = await getEntries();
+
+	await sleep(1_000);
+
+	return (
+		<>
+			<form
+				action={async formData => {
+					"use server";
+
+					const entry = formData.get("text") as string;
+
+					await database.insert(entries).values({ id: `${Date.now()}`, text: entry });
+
+					revalidateTag("entries");
+				}}
+			>
+				<input name="text" type="text" />
+				<button type="submit">Submit</button>
+			</form>
+			<div>
+				{data.map(row => (
+					<p key={row.id}>{`${row.id} - ${row.text}`}</p>
+				))}
+			</div>
+		</>
 	);
 };
 
 const HomePage = () => (
 	<>
-		<form
-			action={async formData => {
-				"use server";
-
-				const entry = formData.get("text") as string;
-
-				await database.insert(entries).values({ id: `${Date.now()}`, text: entry });
-
-				revalidatePath("/");
-			}}
-		>
-			<input name="text" type="text" />
-			<span style={css({ "--color": "var(---, blue)" })}>{getRandomNumber()}</span>
-			<button
-				formAction={async () => {
-					"use server";
-
-					await sleep(100);
-
-					revalidateTag("random-number");
-				}}
-				type="submit"
-			>
-				Reset random number
-			</button>
-			<button type="submit">Submit</button>
-		</form>
-		<Suspense fallback={<p>Loading list</p>}>
-			<List />
+		<p style={css({ "--color": "var(---, red)" })}>Hello world</p>
+		<Suspense fallback={<p>Loading entries data...</p>}>
+			<Entries />
+		</Suspense>
+		<Suspense fallback={<p>Loading user data...</p>}>
+			<User />
 		</Suspense>
 	</>
 );
