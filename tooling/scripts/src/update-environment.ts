99477f63cdbab7606242a8f8ae40e14501ea1347
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-04-17 18:23:18 +0200

    Add missing space

--- tooling/scripts/src/update-environment.ts
+++ tooling/scripts/src/update-environment.ts
@@ -10,4 +10,4 @@ await $`cp apps/sqlite-proxy/fly.toml .`;
 await $`flyctl deploy --app=${getSqliteProxyAppName()} --remote-only --build-secret TURBO_TEAM=${CONFIG.TURBO_TEAM} --build-secret TURBO_TOKEN=${CONFIG.TURBO_TOKEN} --yes`;
 
 await $`cp apps/website/fly.toml .`;
-await $`flyctl deploy --app=${getWebsiteAppName()} --remote-only --build-secret TURBO_TEAM=${CONFIG.TURBO_TEAM} --build-secret TURBO_TOKEN=${CONFIG.TURBO_TOKEN}--build-secret SQLITE_PROXY_URL=${getSqliteProxyInternalUrl()} --yes`;
+await $`flyctl deploy --app=${getWebsiteAppName()} --remote-only --build-secret TURBO_TEAM=${CONFIG.TURBO_TEAM} --build-secret TURBO_TOKEN=${CONFIG.TURBO_TOKEN} --build-secret SQLITE_PROXY_URL=${getSqliteProxyInternalUrl()} --yes`;
