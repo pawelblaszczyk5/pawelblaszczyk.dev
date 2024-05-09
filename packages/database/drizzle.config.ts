491f71396c076dbdf0e038177e6b91341cd82cc1
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-05-09 21:14:48 +0200

    Add comment

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "drizzle-kit";
 
 import { defineConfig } from "drizzle-kit";
 
+// TODO[+effect] Effect should clean this up a lot 😄
 const dbCredentials = {
 	url: process.env["TURSO_URL"] ?? "file:../../apps/website/local/sqlite.db",
 	...(process.env["TURSO_AUTH_TOKEN"] ? { authToken: process.env["TURSO_AUTH_TOKEN"] } : undefined),
