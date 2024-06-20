adea2032d2935b885cde51e7a61a5d7ad3925194
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-06-20 15:41:47 +0200

    Simplify drizzle config

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,15 +1,10 @@
-import type { Config } from "drizzle-kit";
-
 import { defineConfig } from "drizzle-kit";
 
-// TODO[+effect] Effect should clean this up a lot 😄
-const dbCredentials = {
-	url: process.env["TURSO_URL"] ?? "file:../../apps/website/local/sqlite.db",
-	...(process.env["TURSO_AUTH_TOKEN"] ? { authToken: process.env["TURSO_AUTH_TOKEN"] } : undefined),
-} satisfies Extract<Config, { dialect: "sqlite"; driver: "turso" }>["dbCredentials"];
-
 export default defineConfig({
-	dbCredentials,
+	// NOTE this is only used for local push, migrations are handled via runtime migrator during application startup
+	dbCredentials: {
+		url: "file:../../apps/website/local/sqlite.db",
+	},
 	dialect: "sqlite",
 	driver: "turso",
 	out: "./drizzle",
